8b2055ad3e5bf7e793533d53a92c13336f4ae23e
Jesus Aguirre <infoaguirrejesus@duck.com>
2023-07-09 19:03:03 -0500

    Manejo en path de endpoints

--- src/main/java/com/upaio/GreetingResource.java
+++ src/main/java/com/upaio/GreetingResource.java
@@ -1,18 +1,19 @@
 package com.upaio;
 
+import jakarta.ws.rs.ApplicationPath;
 import org.jboss.logging.Logger;
 
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
-
-@Path("/hello")
+@ApplicationPath("api")
 public class GreetingResource {
 
     private static final Logger LOG = Logger.getLogger(GreetingResource.class);
 
     @GET
+    @Path("/hello")
     @Produces(MediaType.APPLICATION_JSON)
     public String hello() {
         LOG.debug("Hi from debug");
