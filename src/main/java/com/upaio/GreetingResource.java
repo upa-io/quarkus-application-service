079671a8f2ae607fb33e11886f662a3a32f251f6
Jesus Aguirre <infoaguirrejesus@duck.com>
2023-07-09 19:14:00 -0500

    Ajuste en endpoint

--- src/main/java/com/upaio/GreetingResource.java
+++ src/main/java/com/upaio/GreetingResource.java
@@ -1,19 +1,17 @@
 package com.upaio;
 
-import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.Path;
 import org.jboss.logging.Logger;
 
 import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
-@ApplicationPath("api")
+@Path("api")
 public class GreetingResource {
 
     private static final Logger LOG = Logger.getLogger(GreetingResource.class);
 
     @GET
-    @Path("/hello")
     @Produces(MediaType.APPLICATION_JSON)
     public String hello() {
         LOG.debug("Hi from debug");
