6d4f078531105a622b45550750ce6cdb5487621d
Jesus Aguirre <infoaguirrejesus@duck.com>
2023-08-06 16:51:38 -0500

    Crear clases de prueba para objetos DTO

---
+++ src/test/java/com/upaio/models/ServiceRequestGreetingsTest.java
@@ -0,0 +1,49 @@
+package com.upaio.models;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ServiceRequestGreetingsTest {
+
+    @Test
+     void testEqualsAndHashCode() {
+        // Crear dos objetos ServiceRequestGreetings con los mismos valores
+        ServiceRequestGreetings greeting1 = new ServiceRequestGreetings();
+        greeting1.setGreeting("Hi");
+        ServiceRequestGreetings greeting2 = new ServiceRequestGreetings();
+        greeting2.setGreeting("Hi");
+
+        // Verificar que equals() devuelve true cuando los objetos tienen los mismos valores
+        Assertions.assertEquals(greeting1, greeting2);
+
+        // Verificar que hashCode() devuelve el mismo valor para objetos con los mismos valores
+        Assertions.assertEquals(greeting1.hashCode(), greeting2.hashCode());
+    }
+
+    @Test
+     void testToString() {
+        // Crear un objeto ServiceRequestGreetings
+        ServiceRequestGreetings greeting = new ServiceRequestGreetings();
+        greeting.setGreeting("Hi");
+
+        // Verificar que toString() genera la representación correcta del objeto
+        Assertions.assertEquals("ServiceRequestGreetings(greeting=Hi)", greeting.toString());
+    }
+
+    @Test
+     void testGettersAndSetters() {
+        // Crear un objeto ServiceRequestGreetings
+        ServiceRequestGreetings greeting = new ServiceRequestGreetings();
+        greeting.setGreeting("Hi");
+
+        // Verificar que los getters devuelven el valor correcto
+        Assertions.assertEquals("Hi", greeting.getGreeting());
+
+        // Verificar que los setters actualizan correctamente el valor
+        greeting.setGreeting("Hi again");
+        Assertions.assertEquals("Hi again", greeting.getGreeting());
+    }
+
+}
